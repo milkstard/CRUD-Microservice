e444967f401559fe34b8173378cd1b83426aa13d
milkstard <jmarkcarbs@gmail.com>
2025-06-14 21:50:18 +0800

    Initialize dbcontext and add UserAPI controller, services, and models

--- OcelotGatewayExample/UserAPI/Program.cs
+++ OcelotGatewayExample/UserAPI/Program.cs
@@ -1,7 +1,12 @@
+using UserAPI.Data;
+using UserAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("https://localhost:7285");
 // Add services to the container.
-
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddDbContext<DbContextClass>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
