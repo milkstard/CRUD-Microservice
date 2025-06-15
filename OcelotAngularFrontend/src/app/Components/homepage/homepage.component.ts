d2009da1c25b0ff54820210fb6296ede630a2acb
milkstard <jmarkcarbs@gmail.com>
2025-06-15 10:42:55 +0800

    Initialize components

---
+++ OcelotAngularFrontend/src/app/Components/homepage/homepage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ocelot-homepage',
+  standalone: true,
+  imports: [],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.css'
+})
+export class HomepageComponent {
+
+}
