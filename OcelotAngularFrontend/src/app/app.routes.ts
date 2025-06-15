50fc193a8f7f4aee7872ccce30b07a02314a5e29
milkstard <jmarkcarbs@gmail.com>
2025-06-15 11:55:32 +0800

    Update routes

--- OcelotAngularFrontend/src/app/app.routes.ts
+++ OcelotAngularFrontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from '../app/Components/homepage/homepage.component';
+import { ProductsComponent } from '../app/Components/products/products.component';
+import { UsersComponent } from '../app/Components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'users', component: UsersComponent }
+];
