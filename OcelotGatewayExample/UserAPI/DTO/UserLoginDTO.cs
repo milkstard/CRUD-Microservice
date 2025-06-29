5a0287ce74e6b25940e7cd25316ec19d650317ef
milkstard <jmarkcarbs@gmail.com>
2025-06-30 07:32:42 +0800

    Create a DTO for user logni

---
+++ OcelotGatewayExample/UserAPI/DTO/UserLoginDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace UserAPI.DTO
+{
+    public class UserLoginDTO
+    {
+        public string UserName;
+        public string Password;
+        public string Role;
+        public string[] Scopes;
+    }
+}
