23f11bc157771166ab4abd99aae201c2c971d41e
milkstard <jmarkcarbs@gmail.com>
2025-06-30 07:32:07 +0800

    Update User model

--- OcelotGatewayExample/UserAPI/Models/User.cs
+++ OcelotGatewayExample/UserAPI/Models/User.cs
@@ -5,5 +5,8 @@
         public int UserId {  get; set; }
         public string UserName { get; set; }
         public string Address {  get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public string[] Scopes {  get; set; } 
     }
 }
